d9e4567757a81e8407c9824605010801d075cdcf
kento.Hirabayashi <aspirationkent@gmail.com>
2018-02-02 20:55:55 +0900

    課題提出　課題：不要になったアイテムを順次破棄しましょう
    ユニティちゃんが通り過ぎて画面外に出たアイテムを破棄してます。
    DestroyPrefab.csにアイテムを破棄するコードを記述しそれぞれのprefabに貼りつけました。

---
+++ Assets/DestroyPrefab.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyPrefab : MonoBehaviour {
+
+	public GameObject MainCamera;
+	private float difference;
+
+
+	// Use this for initialization
+	void Start () {
+
+		this.MainCamera = GameObject.Find ("MainCamera");
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.difference = this.transform.position.z - MainCamera.transform.position.z;
+		if (this.difference <= 0) {
+			Destroy (gameObject);
+		}
+
+		
+	}
+}
